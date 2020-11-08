3a2ddc780ac79438117354397235af99e668b91b
unknown <xuancuong2503>
2020-11-08 16:50:11 +0700

    them_mon_hoc

---
+++ models/MonHoc.js
@@ -0,0 +1,8 @@
+var monHoc = function(){
+    this.maMonHoc='';
+    this.tenMonHoc='';
+    this.xuatThongTinMonHoc=function(){
+        console.log('mã môn học',$(this.maMonHoc));
+        console.log('ten môn học',$(this.tenMonHoc));
+    }
+}
